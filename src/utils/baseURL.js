cdd15dbf57481e507f94037e904d66249face2cf
Maleek Adebayo <amaleekadedotun@gmail.com>
2025-09-23 00:31:50 +0100

    final project

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,3 +1,3 @@
 // const baseURL = "http://localhost:7000/api/v1";
-const baseURL = 'https://e-commerce2-f4sn.onrender.com/api/v1';
+const baseURL = 'https://e-commerce2-ex5z.onrender.com/api/v1';
 export default baseURL;
