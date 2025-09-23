e5ba5a455f193260937b7628f95a0763611fabd7
Maleek Adebayo <amaleekadedotun@gmail.com>
2025-09-23 01:22:05 +0100

    change navbar logo

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,7 +8,7 @@ import {
 } from "@heroicons/react/24/outline";
 import { Link } from "react-router-dom";
 import baseURL from "../../utils/baseURL";
-import logo from "./logo3.png";
+import logo from "./logo4.png";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchCategoriesAction } from "../../redux/slices/category.js/categorySlice";
 import { getCartItemsFromLocalStorage } from "../../redux/slices/cart/cartSlice";
