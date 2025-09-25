2f95dadd43c8f9ef12d41f6a1e8dd94d65ad69c8
Maleek Adebayo <amaleekadedotun@gmail.com>
2025-09-25 09:58:08 +0100

    final project

--- src/components/AuthRoute/AuthRoute.js
+++ src/components/AuthRoute/AuthRoute.js
@@ -1,33 +1,33 @@
-// import React from 'react'
-// import Login from '../Users/Forms/Login'
-// const AuthRoute = ({children}) => {
-//      // get user from localStorage
-//     const user = JSON.parse(localStorage.getItem("userInfo"))
-//     const isLoggedIn = user?.token ? true : false
+import React from 'react'
+import Login from '../Users/Forms/Login'
+const AuthRoute = ({children}) => {
+     // get user from localStorage
+    const user = JSON.parse(localStorage.getItem("userInfo"))
+    const isLoggedIn = user?.token ? true : false
 
-//     if(!isLoggedIn) return <Login />
-//   return (
-//     <>
-//         {children}  
-//     </>
-//   )
-// }
+    if(!isLoggedIn) return <Login />
+  return (
+    <>
+        {children}  
+    </>
+  )
+}
 
-// export default AuthRoute
+export default AuthRoute
 
 
-import { Navigate } from "react-router-dom";
+// import { Navigate } from "react-router-dom";
 
-const AuthRoute = ({ children }) => {
-  const user = JSON.parse(localStorage.getItem("userInfo"));
-  const isLoggedIn = user?.token ? true : false;
+// const AuthRoute = ({ children }) => {
+//   const user = JSON.parse(localStorage.getItem("userInfo"));
+//   const isLoggedIn = user?.token ? true : false;
 
-  if (!isLoggedIn) {
-    // If not logged in → redirect to login
-    return <Navigate to="/login" replace />;
-  }
+//   if (!isLoggedIn) {
+//     // If not logged in → redirect to login
+//     return <Navigate to="/login" replace />;
+//   }
 
-  return children;
-};
+//   return children;
+// };
 
-export default AuthRoute;
+// export default AuthRoute;
